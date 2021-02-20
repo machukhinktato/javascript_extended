4f912a2556ed916246782f9564174ab7c53407b7
Mikhail Tarabrin <51959562+machukhinktato@users.noreply.github.com>
2021-02-20 16:48:42 +0300

    Update script.js

--- mainapp/script.js
+++ mainapp/script.js
@@ -1,14 +1,14 @@
 const goods = [
-    {title: 'Shirt', price: 150},
-    {title: 'Socks', price: 20},
-    {title: 'jacket', price: 350},
-    {title: 'Shoes', price: 250},
+    {title: 'Shirt', price: 150, image: "https://bpm.hse.ru/Runtime/site/img/logo.svg"},
+    {title: 'Socks', price: 20,image: "https://bpm.hse.ru/Runtime/site/img/logo.svg"},
+    {title: 'jacket', price: 350,image: "https://bpm.hse.ru/Runtime/site/img/logo.svg"},
+    {title: 'Shoes', price: 250,image: "https://bpm.hse.ru/Runtime/site/img/logo.svg"},
 ];
 
-const renderGoodsItem = (title, price) => `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
+const renderGoodsItem = (title, price, image) => `<div class="goods-item"><img src="${image}" alt="picture" class="img"><h3>${title}</h3><p>${price} usd</p></div>`;
 
 const renderGoodsList = (list) => {
-    let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
+    let goodsList = list.map(item => renderGoodsItem(item.title, item.price, item.image));
     document.querySelector('.goods-list').innerHTML = goodsList.join(' ');
 }
 
