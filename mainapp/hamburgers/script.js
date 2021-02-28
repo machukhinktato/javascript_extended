a37e63a8532eae3a76890b0728e8196f0d38d5dc
machukhinktato <tarabrinmv@gmail.com>
2021-02-28 12:42:31 +0300

    all works project in  progress

--- mainapp/hamburgers/script.js
+++ mainapp/hamburgers/script.js
@@ -1,19 +1,24 @@
 class Hamburger {
     constructor() {
-        this.topping = ['cheese', 'salad', 'potato'];
-        this.listOfCondiment = ['sprinkle with seasoning', 'add mayonnaise']
-        this.condimentIsUsed = false;
-        this.mayonnaisAdded = false;
+        this.toppingList = {'cheese': [10, 20], 'salad': [20, 5], 'potato': [15, 10]};
+        this.condimentList = {'sprinkle with seasoning': [15, 0], 'use mayonnaise': [20, 5]};
+        this.hamburgerSize = {'large': [50, 20], 'small': [100, 40]};
+        this.infoAboutPrice = undefined;
+        this.infoAboutCalories = undefined;
+        this.selectSize()
     }
-    SprinkleWithSeasoning() {
-        if (this.condimentIsUsed === true) {
-            this.condimentIsUsed = false;
-        } else {
-
-        }
-
+    selectSize() {
+        let answer = prompt('small or large?');
+        console.log(this.hamburgerSize[answer]);
+        this.infoAboutPrice = this.hamburgerSize[answer][0];
+        this.infoAboutCalories = this.hamburgerSize[answer][1];
+        this.selectTopping()
     }
-    useMayonnaise() {
-        this.mayonnaisAdded = true;
+    selectTopping() {
+        console.log(this.toppingList);
+        let answer = prompt('with which topping? (cheese, salad, potato)')
+        console.log(this.toppingList[answer]);
+        this.infoAboutPrice += this.toppingList[answer][0]
+        this.infoAboutCalories += this.toppingList[answer][1]
     }
 }
