47a9b7a880967293746947100a4564e589ec8db8
machukhinktato <tarabrinmv@gmail.com>
2021-03-02 19:03:20 +0300

    all works project in  progress

--- mainapp/script.js
+++ mainapp/script.js
@@ -1,3 +1,24 @@
+const API_ROOT = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+const request = (path = '', callback, method = 'GET', body) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                console.log({response: xhr.responseText});
+                callback(JSON.parse(xhr.responseText));
+            } else {
+                console.error(xhr.responseText);
+            }
+        }
+    }
+
+    xhr.open(method, `${API_ROOT}/${path}`);
+
+    xhr.send(body);
+}
+
 class GoodsItem {
     constructor(item) {
         this.item = item;
@@ -5,93 +26,110 @@ class GoodsItem {
 
     render() {
         return `
-        <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="${this.item.image}" alt="Card image cap">
-            <div class="card-body text-center">
-                <h5 class="card-title">${this.item.title}</h5>
-                <p class="card-text">${this.item.price}</p>
-                <a href="#" class="btn btn-outline-secondary">check details</a>
+            <div class="item">
+                <h2>${this.item.product_name}</h2>
+                <p>${this.item.price}</p>
             </div>
-        </div>
         `;
     }
 }
-
 
 class GoodsList {
     constructor() {
         this.goods = [];
     }
 
-    fetchData() {
-        this.goods = [
-            {title: 'Shirt', price: 150, image: "https://a.lmcdn.ru/img600x866/M/P/MP002XW047N4_12804328_1_v1_2x.jpg"},
-            {title: 'Socks', price: 20, image: "https://a.lmcdn.ru/product/M/P/MP002XW04FOT_12975464_1_v1.jpg"},
-            {title: 'Jacket', price: 350, image: "https://a.lmcdn.ru/img600x866/I/X/IX001XW00U8W_13143761_2_v2.jpeg"},
-            {title: 'Shoes', price: 250, image: "https://a.lmcdn.ru/product/D/R/DR004AUKPTG7_11968908_1_v2.jpg"},
-        ];
+    fetchData(callback) {
+        request('catalogData.json', (goods) => {
+            this.goods = goods;
+            callback();
+        });
     }
 
-    totalPrice() {
-        let totalSum = 0;
-        this.goods.forEach(elem => {
-                totalSum += elem.price
-            }
+    getTotalPrice() {
+        const sum = this.goods.reduce(
+            (accumulator, currentElement) => accumulator + currentElement.price,
+            0
         );
-        return totalSum;
-    };
+        console.log(sum)
+    }
 
     render() {
         const goodsString = this.goods.map(element => {
             const item = new GoodsItem(element);
             return item.render();
         });
-        document.querySelector('.goods-list').innerHTML = goodsString.join('');
+        document.querySelector('.goods-list').innerHTML = goodsString.join(' ');
     }
 }
 
-class Basket {
 
+class Basket {
     fetchData() {
         // запрос данных с сервера
     }
 
-    calculateSum() {
-        // общее кол-во товаров и их общая цена
+    addItem() {
+
+
     }
 
-    pay() {
-        // оплатить заказ
+    removeItem() {
+
+    }
+
+    changeQuantity() {
+
+    }
+
+    applyCoupon() {
+
+    }
+
+    getDeliveryPrice() {
+
+    }
+
+    createOrder() {
+
+    }
+
+    clear() {
+
+    }
+
+    getTotalPrice() {
+
     }
 
     render() {
-        // отобразить  корзину
+
     }
 }
+
 
 class BasketItem {
 
-    details() {
-        // возможность взглянуть на каждый товар углубленно
+    changeType() {
+
     }
 
-    incerementQua() {
-        // увеличить уменьшить количество
+    removeItem() {
+
     }
 
-    remove() {
-        // удалить из корзины
-    }
+    changeQuantity() {
 
-    onDelay() {
-        // отложить товар из покупаемых, но продолжать отслеживать его
     }
 
     render() {
-        // отобразить товар в корзине, его стоимость и кол-во повторений позиции
+
     }
 }
 
+
 const list = new GoodsList();
-list.fetchData();
-list.render();
+list.fetchData(() => {
+    list.render();
+    list.getTotalPrice();
+});
