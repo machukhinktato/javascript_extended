20c83a7b1f29ac670b22e53d37193ef9e7a701c6
machukhinktato <tarabrinmv@gmail.com>
2021-03-21 15:22:59 +0300

    in progress

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-let a = 5;
-console.log(a);
+const fs = require('fs');
+fs.readFile('./data.json', 'utf-8', (err, data) => {
+    if (!err) {
+        const obj = JSON.parse(data);
+        obj.third = 'THREE';
+
+        fs.writeFile('./data.json', JSON.stringify(obj), err => {
+
+        })
+    }
+});
