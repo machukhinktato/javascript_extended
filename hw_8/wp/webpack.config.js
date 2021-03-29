9e9f0f63900a57d02d9ec8ad49f49ff7342fe0a3
machukhinktato <tarabrinmv@gmail.com>
2021-03-29 17:32:43 +0300

    in progress

--- hw_8/wp/webpack.config.js
+++ hw_8/wp/webpack.config.js
@@ -1,7 +1,20 @@
+const { VueLoaderPlugin } = require('vue-loader');
+
 module.exports = {
     entry: '../project/static/script.js',
     mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
+        ]
+    },
     output: {
         filename: './bundle.js'
-    }
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
 }
